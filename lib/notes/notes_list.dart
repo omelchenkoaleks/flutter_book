4171bb0e4a119c1d528d418ae7ddd132005ad911
omelchenkoaleks <omelchenkoaleks@gmail.com>
2022-02-14 10:16:16 +0200

    NotesList: custom list to display Cards.

--- lib/notes/notes_list.dart
+++ lib/notes/notes_list.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
+import '../notes/notes_model.dart';
+
+// mock
+List<Note> notesList = [
+  Note('one', 'one one one'),
+  Note('two', 'two two two'),
+  Note('three', 'three three three'),
+  Note('four', 'four four four'),
+  Note('five', 'five five five'),
+];
 
 class NotesList extends StatelessWidget {
   const NotesList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('NOTES'),
+    print("## NotesList.build()");
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: () {},
+      ),
+      body: ListView.builder(
+          itemCount: notesList.length,
+          itemBuilder: (context, int index) {
+            return Container(
+              padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+              child: Card(
+                elevation: 8,
+                child: ListTile(
+                  title: Text(notesList[index].title),
+                  subtitle: Text(notesList[index].content),
+                  onTap: () {},
+                ),
+              ),
+            );
+          }),
     );
   }
 }
