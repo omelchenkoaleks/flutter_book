f8cf0c8b13ccf27587fe6c3574f3cabe4f06145e
omelchenkoaleks <omelchenkoaleks@gmail.com>
2022-02-14 17:06:03 +0200

    delete note from custom list

--- lib/notes/notes_list.dart
+++ lib/notes/notes_list.dart
@@ -32,12 +32,24 @@ class NotesList extends StatelessWidget {
           itemBuilder: (context, int index) {
             Color color = Colors.white;
             switch (notesList[index].color) {
-              case "red" : color = Colors.red; break;
-              case "green" : color = Colors.green; break;
-              case "blue" : color = Colors.blue; break;
-              case "yellow" : color = Colors.yellow; break;
-              case "grey" : color = Colors.grey; break;
-              case "purple" : color = Colors.purple; break;
+              case "red" :
+                color = Colors.red;
+                break;
+              case "green" :
+                color = Colors.green;
+                break;
+              case "blue" :
+                color = Colors.blue;
+                break;
+              case "yellow" :
+                color = Colors.yellow;
+                break;
+              case "grey" :
+                color = Colors.grey;
+                break;
+              case "purple" :
+                color = Colors.purple;
+                break;
             }
             return Container(
               padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
@@ -47,11 +59,50 @@ class NotesList extends StatelessWidget {
                 child: ListTile(
                   title: Text(notesList[index].title),
                   subtitle: Text(notesList[index].content),
-                  onTap: () {},
+                  onTap: () {
+                    _deleteNote(context, notesList[index]);
+                  },
                 ),
               ),
             );
           }),
     );
   }
+
+  Future _deleteNote(BuildContext inContext, Note note) {
+    print("## NotesList._deleteNote(): Note = $note");
+
+    return showDialog(
+      context: inContext,
+      builder: (BuildContext inAlertContext) {
+        return AlertDialog(
+          title : const Text("Delete Note"),
+          content : Text("Are you sure you want to delete ${note.title}?"),
+          actions: [
+            TextButton(child : const Text("Cancel"),
+                onPressed: () {
+                  // Just hide dialog.
+                  Navigator.of(inAlertContext).pop();
+                }
+            ),
+            TextButton(child : const Text("Delete"),
+                onPressed : ()  {
+                  notesList.remove(note);
+                  Navigator.of(inAlertContext).pop();
+                  Scaffold.of(inContext).showSnackBar(
+                      const SnackBar(
+                          backgroundColor : Colors.red,
+                          duration : Duration(seconds : 2),
+                          content : Text("Note deleted")
+                      )
+                  );
+                }
+            ),
+          ],
+        );
+      }
+    );
+
+  }
+
 }
