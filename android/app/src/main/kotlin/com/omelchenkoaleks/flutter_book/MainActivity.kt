84291fd908592287b55820269ce340ae86fcdacf
omelchenkoaleks <omelchenkoaleks@gmail.com>
2022-02-14 08:48:06 +0200

    init

---
+++ android/app/src/main/kotlin/com/omelchenkoaleks/flutter_book/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.omelchenkoaleks.flutter_book
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('My Home Page!'),
+    );
+  }
+}
